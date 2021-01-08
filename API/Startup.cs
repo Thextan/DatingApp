d77dcce28cab451550d8e8026d9f5b416b15f6da
Dustin Keith <Thextan@gmail.com>
2021-01-08 09:21:20 -0600

    end 58 add register form

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,11 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using API.Extensions;
 
 namespace API
